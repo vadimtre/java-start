8a0fd94ac9f008e7fd6544a9ad9ce4e1c7ff64c4
vadimtre <vadimtre@gmail.com>
2018-09-04 17:19:37 +0300

    the beginning of the swim

---
+++ src/MethodTwoArgIntReturnMultiplication.java
@@ -0,0 +1,20 @@
+
+public class MethodTwoArgIntReturnMultiplication{
+public MethodTwoArgIntReturnMultiplication(int first, int second) {
+		// TODO Auto-generated constructor stub
+	System.out.println((long)(first+second));
+	}
+
+/*Task-5-1 #13
+ * Написать мпод, который принимает два аргумента типа int и возвращает результат их умножения как тип long:*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MethodTwoArgIntReturnMultiplication(3, 5);
+	}
+	
+	/*void MethodTwoArgIntReturnMultiplication(int first, int second ) {
+		
+		System.out.println((long)(first+second));
+	}*/
+
+}
